5eed7e6740ca30006c1e227c418fffb4f572dbbd
James <29504374@qq.com>
2020-10-09 17:23:26 +0800

    搭建three基本运行环境。

---
+++ bicycle/README.md
@@ -0,0 +1,10 @@
+## bicycle-view-3D 自行车三维预览系统
+## 技术组成
+###  threeJS、three-orbit-controls
+## 调试工具
+### chorme、Three.js Editor、
+## 打包工具
+### webpack
+
+## 安装方式
+### npm install、npm run watch or build 
---
+++ bicycle/src/index.js
@@ -0,0 +1,81 @@
+import * as THREE from 'three';
+
+const OrbitControls = require('three-orbit-controls')(THREE);
+var container;
+var renderer;
+var scene;
+var camera;
+var light;
+var gridHelper;
+var orbitControls;
+
+function init() {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    globalOnStart();
+}
+//threeJS 初始化
+function globalOnStart() {
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true,
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+    globalAddedScene();
+    globalAddedCamera();
+    globalAddedlight();
+    globalAddedGrid();
+    globalAddedOrbitControls();
+}
+//添加场景
+function globalAddedScene() {
+    scene = new THREE.Scene();
+}
+//添加全局摄像机
+function globalAddedCamera() {
+    camera = new THREE.PerspectiveCamera(45,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    )
+    camera.lookAt(scene.position);
+    camera.position.set(-10, 50, 50);
+}
+//添加全局灯光
+function globalAddedlight() {
+    light = new THREE.DirectionalLight(0xffffff, 1);
+    scene.add(light);
+}
+//添加辅助网格
+function globalAddedGrid() {
+    gridHelper = new THREE.GridHelper(window.innerWidth,
+        window.innerHeight,
+        0x000000,
+        0x000000
+    )
+    gridHelper.material.opacity = 0.5;
+    gridHelper.material.transparent = true;
+    scene.add(gridHelper);
+    var axesHelper = new THREE.AxesHelper(15 );
+scene.add( axesHelper );
+}
+//渲染
+function globalOnRender() {
+    renderer.render(scene, camera);
+}
+//一直渲染
+function globalGoAnimate() {
+    requestAnimationFrame(globalGoAnimate);
+    globalOnRender();
+}
+//基础辅助工具
+function globalAddedOrbitControls() {
+    orbitControls = new OrbitControls(camera);
+    orbitControls.enableDamping = true
+    orbitControls.dampingFactor = 0.25
+    orbitControls.enableZoom = false
+}
+init();
+globalGoAnimate();
